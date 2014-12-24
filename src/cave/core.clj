ca88c0b05ff534d9b937c2cc87a5d16a55ab1b87
Keith R. McFarlane <lochsloy1745@yahoo.com>
2014-12-23 23:50:21 -0800

    New map details

--- src/cave/core.clj
+++ src/cave/core.clj
@@ -5,43 +5,63 @@
 (require '[clojure.string :as str])
 
 
-(defn load-map []
-  { :great-hall {:name "Great Hall" :items ["Sword" "Spear" "Chicken McNuggets"] s: :study e: :master-bedroom} :master-bedroom {:name "Master Bedroom" w: :great-hall s: kitchen w: :master-bathroom} :master-bathroom {:name "Master Bathroom"}
-   :study {:name "Study"} :kitchen {:name "Kitchen"} :guest-bedroom {:name "Guest Bedroom"}
-   :gasrage {:name "Garage"} :laundry-room {:name "Laundry Room"} :guest-bathroom {:name "Guest Bathroom"}
-    }
+( defn load-map []
+  { :great-hall {:name "Great Hall" :items ["Sword" "Spear" "Chicken McNuggets"] :s :study :e :master-bedroom}
+   :master-bedroom {:name "Master Bedroom" :w :great-hall :s :kitchen :e :master-bathroom}
+   :master-bathroom {:name "Master Bathroom"}
+   :study {:name "Study" :s :great-hall}
+   :kitchen {:name "Kitchen"}
+   :guest-bedroom {:name "Guest Bedroom"}
+   :garage {:name "Garage"}
+   :laundry-room {:name "Laundry Room"}
+   :guest-bathroom {:name "Guest Bathroom"}
+   })
+
+( defn load-map-2 []
+
+  { :town-square {:name "Town Square" :e :great-hall}
+    :great-hall  {:name "Great Hall" :w :town-square :e :throne-room :n :master-bedroom :s :study}
+    :master-bedroom {:name "Master Bedroom" :e :master-bathroom :s :great-hall}
+    :study {:name "Study" :n :great-hall}
+    :throne-room {:name "Throne Room" :w :great-hall}
+    :master-bathroom {:name "Master Bathroom" :w :master-bedroom}
+   }
+  
+  )
 
 
-  (defn process-command [cmd]
-    (let [clean-cmd (str/trim (str/lower-case cmd))]
-      (cond 
-       (= clean-cmd "exit") :exit
-       (= clean-cmd "north") :north
-       (= clean-cmd "south") :south
-       (= clean-cmd "east") :east
-       (= clean-cmd "west") :west
-       :else :unrecognized))))
+
+(defn process-command [cmd]
+  (let [clean-cmd (str/trim (str/lower-case cmd))]
+    (cond 
+     (= clean-cmd "exit") :exit
+     (= clean-cmd "north") :n
+     (= clean-cmd "south") :s
+     (= clean-cmd "east") :e
+     (= clean-cmd "west") :w
+     :else :unrecognized)))
 
 
 (defn eval-loop []
       (loop [m (load-map) 
-      	     a (atom ((m (rand-int 3)) (rand-int 3)))] 
+      	     a (atom :great-hall) ] 
 	       (do
-      	       (println (format "(%s)" (:name @a)))
-               (print "Your command, sire? ")
-	       (flush)
-	       (let [c (count m) 
-	       	     r (count (first m))
-		     ln (read-line) 
-		     cmd (process-command ln)]
-		     (cond
-                      (= cmd :room-change)
-                      (let [rndc (rand-int c) rndr (rand-int r) itm ] (swap! a (fn [x] ((m rndc ) rndr))))
-			(= cmd :exit) (println "Goodbye!")
-			:else (println "Unrecognized command"))
+                 (println (format "(%s)" (:name (@a m))))
+                 (print "Your command, sire? ")
+	         (flush)
+	         (let [ln (read-line) 
+		       cmd (process-command ln)]
+		       (cond
+                        (contains? #{:n :s :e :w} cmd) (swap! a (fn [x] (cmd (@a m))))
+                      	 (= cmd :exit) (println "Goodbye!")
+			 :else (println "Unrecognized command"))
 
-               	    (if (not (= cmd :exit))
-	       	    	(recur m a))))))
+               	         (if (not (= cmd :exit))
+                           (recur m a))
+                 )
+               )
+       )
+)
 
 
 
